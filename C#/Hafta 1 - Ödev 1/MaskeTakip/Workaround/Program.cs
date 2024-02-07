67a96157457705ce8da89e20501819d4b8d7faa6
Eray Şahin <sahineray6060@gmail.com>
2024-02-07 17:43:11 +0300

    C# Kampı

---
+++ C#/Hafta 1 - Ödev 1/MaskeTakip/Workaround/Program.cs
@@ -0,0 +1,12 @@
+﻿using Business.Concrete;
+using Entities.Concretes;
+
+Person person = new();
+
+person.FirstName = "Eray";
+person.LastName = "Şahin";
+person.DateOfBirthYear = 2003;
+person.NationalIdentity = 11223344556;
+
+PttManager pttManager = new PttManager(new PersonManager());
+pttManager.GiveMask(person);
